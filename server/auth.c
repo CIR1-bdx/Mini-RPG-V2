0e357bc54e99871d325c0be79bf4fb98134c9b3f
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2023-12-19 10:12:20 +0100

    Update auth.c

--- server/auth.c
+++ server/auth.c
@@ -9,10 +9,30 @@ void read_auth(){
         return;
     }
 
-    char buf[100];
-    char buf1[100];
-    while (fscanf(ptr, "%s;%s ", buf,buf1) == 1)
-        printf("%s\n", buf);
+    char buffer[100];
+    long long* read_array[1024][2];
+    fscanf(ptr, "%s", buffer);
+    char *token = strtok(buffer, ";");
+    for (int i = 0; token != NULL; ++i) {
+        printf("token : %s\n", token);
+        read_array[i][0] = token;
+        printf("%s -", read_array[i][0]);
+        token = strtok(NULL, ";");
+    }
+    fscanf(ptr, "%s", buffer);
+    token = strtok(buffer, ";");
+    for (int i = 0; token != NULL; ++i) {
+        printf("%s\n",token);
+        read_array[i][1] = token;
+        printf("> %s\n", read_array[i][1]);
+        token = strtok(NULL, ";");
+    }
+    printf("tabl : %s\n", read_array[1][0]);
+
+    for (int i = 0; i < 1024; ++i) {
+        printf("%s -> %s\n", read_array[i][0],read_array[i][1]);
+    }
+
     return;
 }
 
