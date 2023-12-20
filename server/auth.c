7dae180ed6ffe85abc5c959b982122e97bc6b3d8
Charles-Antoine <charto.bta@gmail.com>
2023-12-20 10:55:38 +0100

    autch.c codé

--- server/auth.c
+++ server/auth.c
@@ -1,38 +1,105 @@
-//
-// Created by arno.dubois on 18-Dec-23.
-//
+int ID = 0;
 
-void read_auth(){
-    FILE* ptr = fopen("../debora.csv", "r");
+typedef struct TableauCompte {
+    int id;
+    char pseudo[1024];
+    char motDePasse[1024];
+    struct TableauCompte *next;
+} TableauCompte;
+
+void ajouter(TableauCompte **list, char name[], char mdp[]) {
+    TableauCompte *item = malloc(sizeof(TableauCompte));
+    item->id = ID;
+    ID++;
+    strcpy(item->pseudo, name);
+    strcpy(item->motDePasse, mdp);
+    item->next = *list;
+    *list = item;
+}
+
+char *pseudo_use(TableauCompte *list, char newName[]) {
+    if (list == NULL)return NULL;
+    while (list->next != NULL) {
+        if (strcmp(list->pseudo, newName) == 0) {
+            return list->motDePasse;
+        }
+        list = list->next;
+    }
+    if (strcmp(list->pseudo, newName) == 0) {
+        return list->motDePasse;
+    }
+    return NULL;
+}
+
+int read_auth(char enter[], void *client_socket) {
+    SOCKET socket = *(SOCKET *) client_socket;
+
+    char pseudo[1024] = "";
+    for (int i = 0; i < strlen(enter)-1; ++i) {
+        pseudo[i] = enter[i];
+    }
+
+    TableauCompte *listeCompte = NULL;
+    FILE *ptr = fopen("../debora.csv", "r+");
     if (ptr == NULL) {
         perror(ptr);
-        return;
+        return 1;
     }
 
     char buffer[100];
-    long long* read_array[1024][2];
-    fscanf(ptr, "%s", buffer);
-    char *token = strtok(buffer, ";");
-    for (int i = 0; token != NULL; ++i) {
-        printf("token : %s\n", token);
-        read_array[i][0] = token;
-        printf("%s -", read_array[i][0]);
-        token = strtok(NULL, ";");
-    }
-    fscanf(ptr, "%s", buffer);
-    token = strtok(buffer, ";");
-    for (int i = 0; token != NULL; ++i) {
-        printf("%s\n",token);
-        read_array[i][1] = token;
-        printf("> %s\n", read_array[i][1]);
-        token = strtok(NULL, ";");
-    }
-    printf("tabl : %s\n", read_array[1][0]);
-
-    for (int i = 0; i < 1024; ++i) {
-        printf("%s -> %s\n", read_array[i][0],read_array[i][1]);
+    char *token;
+    char name[1024];
+    char mdp[1024] = "";
+    while (fscanf(ptr, "%s", buffer) != -1) {
+        token = strtok(buffer, ";");
+        for (int i = 0; token != NULL; ++i) {
+            if (i == 0) {
+                strcpy(name, token);
+            } else {
+                strcpy(mdp, token);
+            }
+            token = strtok(NULL, ";");
+        }
+        ajouter(&listeCompte, name, mdp);
     }
 
-    return;
+
+    char *pointer = pseudo_use(listeCompte, pseudo);
+    if (pointer == NULL) {
+        char mdp[1024] = "";
+        strcpy(name, pseudo);
+        if (send(socket, "\02Nouveau compte\nEntrer mdp: ", strlen("\02Nouveau compte\nEntrer mdp: "), 0) == -1) {
+            perror("Erreur lors de l'envoi du message");
+            return 1;
+        }
+        int bytes_received = recv(socket, mdp, sizeof(mdp), 0);
+        if (bytes_received <= 0) {
+            return 0;
+        }
+        ajouter(&listeCompte, name, mdp);
+        fprintf(ptr,"");
+        fprintf(ptr, "%s;%s", name, mdp);
+    } else {
+        while (1){
+            char mdp[1024] = "";
+            if (send(socket, "\02Entrez votre mdp: ", strlen("\02Entrez votre mdp: "), 0) == -1) {
+                perror("Erreur lors de l'envoi du message");
+                return 1;
+            }
+            int bytes_received = recv(socket, mdp, sizeof(mdp), 0);
+            if (bytes_received <= 0) {
+                return 1;
+            }
+            char mot[1024] = "";
+            for (int i = 0; i < strlen(mdp)-1; ++i) {
+                mot[i] = mdp[i];
+            }
+            if (strcmp(mot, pointer) == 0) {
+                break;
+            }
+        }
+    }
+    fclose(ptr);
+
+    return 0;
 }
-
--- server/net/handle_client.c
+++ server/net/handle_client.c
@@ -9,151 +9,28 @@
 #ifdef _WIN32
 DWORD WINAPI handle_client(LPVOID client_socket) {
 #else
-    void* handle_client(void* client_socket) {
+
+void *handle_client(void *client_socket) {
 #endif
-    SOCKET socket = *(SOCKET*)client_socket;
+    SOCKET socket = *(SOCKET *) client_socket;
     char buffer[BUFFER_SIZE];
-    char pseudo[32];
-    //srand(time(NULL));
-//int random = rand();
-
-
-
-
-    while (1) {
-        strcpy(buffer, "\02Avez vous un compte ?(oui/non)\n");
-        send(socket, buffer, strlen(buffer), 0);
-
-        int bytes_received = recv(socket, buffer, BUFFER_SIZE, 0);
+    while (1){
+        if (send(socket, "\02Quel est votre pseudo", strlen("\02rQuel est votre pseudo"), 0) == -1) {
+            perror("Erreur lors de l'envoi du message");
+            return 0;
+        }
+        int bytes_received = recv(socket, buffer, sizeof(buffer), 0);
         if (bytes_received <= 0) {
-            break;
+            return 0;
         }
-        buffer[bytes_received] = '\0';
-
-        if(strcmp(buffer, "oui\n") == 0){
-            while(1) {
-                strcpy(buffer, "\02Quelle est votre pseudo ?\n");
-                send(socket, buffer, strlen(buffer), 0);
-
-                bytes_received = recv(socket, buffer, BUFFER_SIZE, 0);
-                if (bytes_received <= 0) {
-                    break;
-                }
-                buffer[bytes_received] = '\0';
-
-                /*   int i,j;
-                *   i = 0;
-                *   while(i<strlen(buffer)){
-                *       if (buffer[i]==";") {
-                *           for (j=i; j<strlen(buffer); j++)
-                *               buffer[j]=buffer[j+1];
-                *       } else i++;
-                *   }
-
-                * if(pseudoExist(buffer)) {
-                 * strcpy(pseudo, buffer);
-                 * break;
-                 * } else {
-                 *  printf("Ce pseudo n'existe pas\n");
-                 * }
-                 *
-                 */
-                break;
-
-            }
-            /* while(1) {
-            *      strcpy(buffer, "\02Quelle est votre mot de passe ?\n");
-            *      send(socket, buffer, strlen(buffer), 0);
-            *
-            *      bytes_received = recv(socket, buffer, BUFFER_SIZE, 0);
-            *      if (bytes_received <= 0) {
-            *             break;
-            *      }}
-            *      buffer[bytes_received] = '\0';
-            *
-             *   int i,j;
-                *   i = 0;
-                *   while(i<strlen(buffer)){
-                *       if (buffer[i]==";") {
-                *           for (j=i; j<strlen(buffer); j++)
-                *               buffer[j]=buffer[j+1];
-                *       } else i++;
-                *   }
-                *
-            *      if(mdpCorrect(buffer)){
-             *          break;
-             *      } else {
-             *          printf("Mot de passe incorrecte, veuillez reéssayer\n");
-             *      }
-            *
-            * }
-            */
-
-        } else if(strcmp(buffer, "non\n") == 0){
-            while(1) {
-                strcpy(buffer, "\02Quelle pseudo voulez vous utiliser ?\n");
-                send(socket, buffer, strlen(buffer), 0);
-
-                bytes_received = recv(socket, buffer, BUFFER_SIZE, 0);
-                if (bytes_received <= 0) {
-                    break;
-                }
-                buffer[bytes_received] = '\0';
-
-                /*   int i,j;
-                *   i = 0;
-                *   while(i<strlen(buffer)){
-                *       if (buffer[i]==";") {
-                *           for (j=i; j<strlen(buffer); j++)
-                *               buffer[j]=buffer[j+1];
-                *       } else i++;
-                *   }
-
-                * if(pseudoExist(buffer) {
-                 *      printf("Ce pseudo existe déjà, veuillez en choisir un autre\n");
-                 * } else {
-                 *      break;
-                 * }
-                 */
-                break;
-            }
-            /*
-             * strcpy(buffer, "\02Quelle mot de passe voulez vous utiliser ?\n");
-             * send(socket, buffer, strlen(buffer), 0);
-             *
-             * bytes_received = recv(socket, buffer, BUFFER_SIZE, 0);
-             * if (bytes_received <= 0) {
-             *      break;
-             *  }
-             *  buffer[bytes_received] = '\0';
-             *
-             *   int i,j;
-                *   i = 0;
-                *   while(i<strlen(buffer)){
-                *       if (buffer[i]==";") {
-                *           for (j=i; j<strlen(buffer); j++)
-                *               buffer[j]=buffer[j+1];
-                *       } else i++;
-                *   }
-             *  createNewUser(pseudo, mdp);
-             */
-
-        } else {
-            strcpy(buffer, "Entrer invalide, veuillez reéssayer\n");
-            send(socket, buffer, strlen(buffer), 0);
-        }
-
-
-        //read_auth();
-
-// Send the message to all connected clients.
-        /*for (int i = 0; i < MAX_CLIENTS; i++) {
-            if (clients[i] != INVALID_SOCKET) {
-                send(clients[i], buffer, bytes_received, 0);
-            }
-        }*/
+        if (strlen(buffer) <= 4) { continue; }
+        break;
     }
-
+    if (read_auth(buffer,client_socket) == 1){
+        closesocket(socket);
+        printf("Removed 1 client, destroyed 1 thread\n");
+        return 0;
+    }
 
     closesocket(socket);
     printf("Removed 1 client, destroyed 1 thread\n");
